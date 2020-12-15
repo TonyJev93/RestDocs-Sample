dd14505a66b188bd9ba221230b2e75bce569d6ba
tonyjev93 <tonyjev93@gmail.com>
2020-12-16 06:49:06 +0900

    merge

--- src/main/java/com/tonyjev93/restdocs/ex/presentation/api/RestDocsTestResponseDto.java
+++ src/main/java/com/tonyjev93/restdocs/ex/presentation/api/RestDocsTestResponseDto.java
@@ -3,18 +3,13 @@ package com.tonyjev93.restdocs.ex.presentation.api;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
-
-<<<<<<< HEAD
 import java.util.HashMap;
 import java.util.Map;
 
-=======
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
 @Getter
 @Setter
 public class RestDocsTestResponseDto {
     @Getter
-<<<<<<< HEAD
     public static class Res extends RepresentationModel { // HATOAS를 위한 Class 상속
         Map<String, String> contact = new HashMap<>();
 
@@ -22,9 +17,5 @@ public class RestDocsTestResponseDto {
             this.contact.put("name", "JUN");
             this.contact.put("email", "allstarz12@lotte.net");
         }
-=======
-    public static class Res extends RepresentationModel {
-        public Res() {}
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
     }
 }
--- src/test/java/com/tonyjev93/restdocs/ex/presentation/api/RestDocsTestControllerTest.java
+++ src/test/java/com/tonyjev93/restdocs/ex/presentation/api/RestDocsTestControllerTest.java
@@ -4,16 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-=======
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,11 +16,8 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-<<<<<<< HEAD
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-=======
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,11 +27,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class RestDocsTestControllerTest {
 
     private MockMvc mockMvc;
-<<<<<<< HEAD
+
     private RestDocumentationResultHandler document;
 
     @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) throws Exception {
         this.document = document("{class-name}/{method-name}/{step}/");
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .alwaysDo(print())
@@ -50,18 +41,11 @@ class RestDocsTestControllerTest {
                         .withRequestDefaults(prettyPrint())
                         .withResponseDefaults(prettyPrint()))
                 .build();
-=======
-
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).alwaysDo(print()).apply(documentationConfiguration(restDocumentation)).build();
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
     }
 
     @Test
     public void indexTest() throws Exception {
         // halLinks() : _links 인식
-<<<<<<< HEAD
         this.mockMvc.perform(get("/").accept(MediaTypes.HAL_JSON))
                 .andExpect(status().isOk())
                 .andDo(this.document.document(
@@ -76,11 +60,6 @@ class RestDocsTestControllerTest {
                         )
                         )
                 );
-=======
-        this.mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("index", links(halLinks(), linkWithRel("self").description("Link to the self resource"), linkWithRel("alpha").description("Link to the alpha resource"),
-                        linkWithRel("bravo").description("Link to the bravo resource"))));
->>>>>>> 9103aad96bb0a34f86d130c51c64a4334e96a6cf
     }
+
 }
